f5ffe0540320e25d67b76858a5081113ac830c51
guilhermeaeme <guilhermeaeme@gmail.com>
2019-02-19 21:43:47 -0300

    mail

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const passport = require('passport');
+const nodemailer = require('nodemailer');
 const router = express.Router();
 const User = require("../models/User");
 
@@ -55,12 +56,51 @@ router.post("/signup", (req, res, next) => {
 
     newUser.save()
     .then(() => {
+      var transporter = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "3df17404d7da23",
+          pass: "9321da3ba64039"
+        }
+      });
+
+      transporter.sendMail({
+        from: '"My Awesome Project 👻" <myawesome@project.com>',
+        to: email,
+        subject: 'Awesome Subject',
+        text: `Welcome, access http://localhost:3000/auth/confirm/${confirmationCode} to active your account`,
+        html: `Welcome, access <a href="http://localhost:3000/auth/confirm/${confirmationCode}" target="_blank">http://localhost:3000/auth/confirm/${confirmationCode}</a> to active your account`
+      })
+      .then(info => {
+        console.log('nodemailer info', info);
+        //res.redirect("/")
+      })
+      .catch(error => {
+        console.log('nodemailer error', error);
+        //res.redirect("/");
+      });
+
       res.redirect("/");
     })
     .catch(err => {
       res.render("auth/signup", { message: "Something went wrong. Details: " + err });
     })
   });
+});
+
+router.get("/confirm/:confirmationCode", (req, res) => {
+  let confirmationCode = req.params.confirmationCode;
+
+  User.findOneAndUpdate({confirmationCode}, {
+    status: 'Active'
+  }, { new: true })
+  .then(user => {
+    res.render('auth/confirmation');
+  })
+  .catch(err => {
+    console.log(err);
+  })
 });
 
 router.get("/logout", (req, res) => {
